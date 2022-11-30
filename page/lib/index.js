62d0d5b34ba6f2c8ede16617a526e1ebe118bb83
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-30 17:00:05 +0100

    change temporary code

--- page/lib/index.js
+++ page/lib/index.js
@@ -20,9 +20,6 @@ dsa.buttons.addNew("New Test", () => dsa.addCharacter({
   "heading.title": "Test Character",
   "dsa.theme": "black-and-white",
   "dsa.plugins": [
-    "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log(import.meta)"),
+    "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log('loaded a test plugin in a data uri')"),
   ],
-  "dsa.plugins.offline-files": {
-    data: "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log(import.meta)"),
-  },
 }));
