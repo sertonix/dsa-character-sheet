aa15aee1a9be606db0f61e09fd8ede93cc3fb962
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-30 17:03:47 +0100

    add css to close button plugin

--- page/plugins/close-button/index.js
+++ page/plugins/close-button/index.js
@@ -10,6 +10,14 @@ export function addCharacter(character) {
   const element = document.createElement("dsa-button");
   element.classList.add("dsa-character-close");
   element.addEventListener( "click", getRemoveCharacter(character) );
+  element.innerHTML = `\
+    <svg version="1.1" viewBox="0 0 100 100">
+      <path d="M 0,0 100,100"/>
+      <path d="M 100,0 0,100"/>
+    </svg>
+  `;
   character.appendToTop(element);
   return getDisposableFromElement(element);
 }
+
+export const styleURL = "./index.css";
