71e2be7ed05c6206c43e466a890a36daea1a2d96
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-01 12:20:27 +0100

    add all exports to DSA global

--- page/lib/index.js
+++ page/lib/index.js
@@ -1,13 +1,16 @@
 import {DSA} from "./dsa.js";
 
 window.DSA = {
+  ... await import("./bar.js"),
+  ... await import("./button.js"),
+  ... await import("./character.js"),
+  ... await import("./data-schema.js"),
   ... await import("./data.js"),
+  ... await import("./dsa.js"),
+  ... await import("./plugin.js"),
   ... await import("./style.js"),
   ... await import("./theme.js"),
-  ... await import("./plugin.js"),
   ... await import("./utils.js"),
-  ... await import("./character.js"),
-  ... await import("./dsa.js"),
 };
 
 const dsa = window.dsa = new DSA();
