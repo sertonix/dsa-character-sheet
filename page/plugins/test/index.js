f8a2cf59c0a36b2ef61352b1500f4290e59bd6d5
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-05 16:34:17 +0100

    change test theme

--- page/plugins/test/index.js
+++ page/plugins/test/index.js
@@ -4,7 +4,7 @@ await importP("./more.js",import.meta.url);
 
 dsa.buttons.addNew("New Test", () => dsa.addCharacter({
   "heading.title": "Test Character",
-  "dsa.theme": "black-and-white",
+  "dsa.theme": "dark-minimal",
   "dsa.plugins": [
     "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log('loaded a test plugin in a data uri')"),
   ],
