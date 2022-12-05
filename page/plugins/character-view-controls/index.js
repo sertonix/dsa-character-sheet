3f9817a9913d972560676676f6ab1c0f7e140397
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-05 10:08:20 +0100

    add maximize button

--- page/plugins/character-view-controls/index.js
+++ page/plugins/character-view-controls/index.js
@@ -2,21 +2,57 @@ function getRemoveCharacter(character) {
   return () => dsa.removeCharacter(character);
 }
 
+function addCloseButton(character) {
+  const element = document.createElement("dsa-button");
+  element.classList.add("dsa-character-close");
+  element.innerHTML = `\
+    <svg version="1.1" viewBox="0 0 100 100">
+      <path d="M 0,0 100,100"/>
+      <path d="M 100,0 0,100"/>
+    </svg>
+  `;
+  character.topBar.appendToRight(element);
+
+  return new DSA.Disposables(
+    DSA.getDisposableEventListener(element, "click", getRemoveCharacter(character)),
+    DSA.getDisposableElement(element),
+  );
+}
+
+function getToggleMaximize(element) {
+  return () => {
+    if (element.hasAttribute("maximized")) {
+      element.removeAttribute("maximized");
+    } else {
+      element.setAttribute("maximized","");
+    }
+  };
+}
+
+function addMaximizeButton(character) {
+  const element = document.createElement("dsa-button");
+  element.classList.add("dsa-character-maximize");
+  element.innerHTML = `\
+    <svg version="1.1" viewBox="0 0 100 100">
+      <path d="m66.666 100h33.334v-33.334"/>
+      <path d="m0 66.666v33.334h33.333"/>
+      <path d="m66.666 0h33.334v33.333"/>
+      <path d="M 33.333,0 H 0 v 33.333"/>
+    </svg>
+  `;
+  character.topBar.appendToRight(element);
+
+  return new DSA.Disposables(
+    DSA.getDisposableEventListener(element, "click", getToggleMaximize(character.element.main)),
+    DSA.getDisposableElement(element),
+  );
+}
+
 export default {
   addCharacter(character) {
-    const element = document.createElement("dsa-button");
-    element.classList.add("dsa-character-close");
-    element.innerHTML = `\
-      <svg version="1.1" viewBox="0 0 100 100">
-        <path d="M 0,0 100,100"/>
-        <path d="M 100,0 0,100"/>
-      </svg>
-    `;
-    character.topBar.appendToRight(element);
-
     return new DSA.Disposables(
-      DSA.getDisposableEventListener(element, "click", getRemoveCharacter(character)),
-      DSA.getDisposableElement(element),
+      addCloseButton(character),
+      addMaximizeButton(character),
     );
   },
   styleURL: "./index.css",
