47d7b5e7efd2fe72195f3c1089558d1589e7cee7
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-05 14:49:51 +0100

    property for bar element position

--- page/lib/bar.js
+++ page/lib/bar.js
@@ -2,12 +2,15 @@
 export class HorizontalBar {
   element = {
     main: document.createElement("dsa-horizontal-bar"),
-    left: document.createElement("dsa-horizontal-bar-left"),
-    middle: document.createElement("dsa-horizontal-bar-middle"),
-    right: document.createElement("dsa-horizontal-bar-right"),
+    left: document.createElement("dsa-horizontal-bar-e"),
+    middle: document.createElement("dsa-horizontal-bar-e"),
+    right: document.createElement("dsa-horizontal-bar-e"),
   };
 
   constructor() {
+    this.element.left.setAttribute("pos","left");
+    this.element.middle.setAttribute("pos","middle");
+    this.element.right.setAttribute("pos","right");
     this.append(
       this.element.left,
       this.element.middle,
