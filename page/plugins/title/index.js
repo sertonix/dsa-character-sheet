7cab2306753eaed24c4910e9260b29289472b4d8
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-05 17:06:11 +0100

    rename heading plugin to title

--- page/plugins/title/index.js
+++ page/plugins/title/index.js
@@ -4,14 +4,14 @@ function getDisposableFromElement(element) {
 
 export default {
   addCharacter(character) {
-    const element = document.createElement("dsa-character-heading");
-    character.data.onDidChange("heading.title", title => element.innerText = title );
-    element.innerText = character.data.get("heading.title"); // TODO on title change
+    const element = document.createElement("dsa-character-title");
+    character.data.onDidChange("title.title", title => element.innerText = title );
+    element.innerText = character.data.get("title.title");
     character.topBar.appendToMiddle(element);
     return getDisposableFromElement(element);
   },
   dataSchema: {
-    "heading.title": {
+    "title.title": {
       type: "string",
       default: "Character",
     }
