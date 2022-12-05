aa447ceafe70e086183714a9f70761a6d6ca3799
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-05 17:07:50 +0100

    update test plugin

--- page/plugins/test/index.js
+++ page/plugins/test/index.js
@@ -3,7 +3,7 @@ import {importP} from "./import.js";
 await importP("./more.js",import.meta.url);
 
 dsa.buttons.addNew("New Test", () => dsa.addCharacter({
-  "heading.title": "Test Character",
+  "title.title": "Test Character",
   "dsa.theme": "dark-minimal",
   "dsa.plugins": [
     "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log('loaded a test plugin in a data uri')"),
