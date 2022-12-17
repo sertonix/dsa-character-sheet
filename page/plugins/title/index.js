9aa2fa7bb163eae421fed8db95ebd49baddf6ba7
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-17 21:38:31 +0100

    use data for title

--- page/plugins/title/index.js
+++ page/plugins/title/index.js
@@ -1,14 +1,8 @@
 export default {
   addCharacter(character) {
     const element = document.createElement("dsa-character-title");
-    character.config.observe("title.title", title => element.innerText = title );
+    element.innerText = character.data["title.title"];
     character.topBar.appendToMiddle(element);
-  },
-  configSchema: {
-    "title.title": {
-      type: "string",
-      default: "Character",
-    }
   },
   styleURI: "./index.css",
 };
