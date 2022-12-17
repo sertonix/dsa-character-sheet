fd6aeaf9b317bf6ec00dcaed6bcbafa6257ed76d
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-17 12:10:00 +0100

    fix typo

--- page/plugins/title/index.js
+++ page/plugins/title/index.js
@@ -1,7 +1,7 @@
 export default {
   addCharacter(character) {
     const element = document.createElement("dsa-character-title");
-    character.data.observer("title.title", title => element.innerText = title );
+    character.data.observe("title.title", title => element.innerText = title );
     character.topBar.appendToMiddle(element);
   },
   dataSchema: {
