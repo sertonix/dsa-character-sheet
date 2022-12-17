8591cead67c5b15080128f47b8eb762c76cb7989
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-12-17 20:29:56 +0100

    remove test plugin

--- page/plugins/test/index.js
+++
@@ -1,7 +0,0 @@
-dsa.buttons.addNew("New Test", () => dsa.addCharacter({
-  "title.title": "Test Character",
-  "dsa.theme": "dark-minimal",
-  "dsa.plugins": [
-    "data:application/javascript;charset=utf-8," + encodeURIComponent("console.log('loaded a test plugin in a data uri')"),
-  ],
-}));
