ac7a45afa4370c480ce4752edbba5ea348302e88
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-06 23:31:33 +0100

    change command naming

--- page/plugins/import-export/index.js
+++ page/plugins/import-export/index.js
@@ -1,7 +1,7 @@
 const CHARACTER_FILE_TYPES = "application/json,.dsa-char";
 const DEFAULT_CHARACTER_FILE_ENDING = ".json";
 
-dsa.commands.add("import-export.export", () => {
+dsa.commands.add("import-export:export", () => {
   const fileContent = JSON.stringify(dsa.data.getAll(),null,2);
   const blob = new Blob([fileContent]);
   const objectURI = URL.createObjectURL(blob);
@@ -15,7 +15,7 @@ dsa.commands.add("import-export.export", () => {
   tempLink.remove();
 });
 
-dsa.commands.add("import-export.import", () => {
+dsa.commands.add("import-export:import", () => {
   const tempInput = document.createElement("input");
   tempInput.setAttribute("type", "file");
   tempInput.setAttribute("multiple", "");
