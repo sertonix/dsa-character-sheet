da68d520c1f83ef252052f73647a399a9dcd6e29
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-06 18:59:29 +0100

    fix build in plugin uri

--- page/lib/plugin.js
+++ page/lib/plugin.js
@@ -58,7 +58,7 @@ export class Plugin {
   }
 
   async makeLoadPromise() {
-    this.exports = await import(URI.join(import.meta.url,"../../plugins",this.uri)); // TODO fix URI.join
+    this.exports = await import(URI.join(import.meta.url,"../plugins/",this.uri));
     this.loaded = true;
     const styleURI = this.getExport("styleURI");
     if (styleURI) this.style = dsa.style.add(URI.join(this.uri,styleURI));
