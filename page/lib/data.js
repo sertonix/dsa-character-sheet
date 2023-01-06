2dee423d46e1f0ab948d31798bd503d2d91ad267
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-06 17:07:20 +0100

    remove safe json parser

--- page/lib/data.js
+++ page/lib/data.js
@@ -1,11 +1,3 @@
-function safeObject(k,v) {
-  return v && typeof v === "object" && !Array.isArray(v) ? Object.setPrototypeOf(v, null) : v;
-}
-
-export function safeJSONParse(str) { // TODO allow reviver
-  return JSON.parse(str,safeObject);
-}
-
 export class DataManager {
   data = Object.create(null);
   observer = Object.create(null);
