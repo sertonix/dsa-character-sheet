84b27cc973e520e5ad8f3f4bf552a3d2fbc4d4d0
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-06 23:22:36 +0100

    add new commanf

--- page/lib/character.js
+++ page/lib/character.js
@@ -35,6 +35,7 @@ export class Character {
     this.element.main.append(this.style.element);
     
     this.commands.add("dsa.about", () => window.open(URI.join(window.location.href,"."), "_blank")?.focus() );
+    this.commands.add("dsa.new", () => window.open(window.location.href, "_blank")?.focus() );
   }
 
   initialize() {
