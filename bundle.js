3ce7a5b968db5875617af3ea52c126e57ff98fc5
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-09 17:34:32 +0100

    organize bundle script

--- bundle.js
+++ bundle.js
@@ -1,37 +1,34 @@
 const fs = require("node:fs");
 const path = require("node:path");
-
-const libFiles = Object.create(null);
-const libPath = path.join(__dirname,"page","lib");
-
-// read file content
-for (const fileName of fs.readdirSync(libPath)) {
-  libFiles[fileName] = fs.readFileSync(path.join(libPath,fileName),{encoding:"utf8"});
-}
-
-let htmlContent = fs.readFileSync(path.join(__dirname,"page","sheet.html"),{encoding:"utf8"});
-
-// bundle favicon
-const favicon = fs.readFileSync(path.join(__dirname,"page","favicon.svg"),{encoding:"utf8"});
-htmlContent = htmlContent.replace("./favicon.svg", "data:image/svg+xml," + encodeURIComponent(favicon) );
-
-// order files so that imports always resolve
 const importRegexp = /^import (.+?) from ".\/([a-zA-Z\-_]*?.js)";$/gm;
-const orderedLibFiles = [];
-let unsortedLibFiles = Object.entries(libFiles);
 
-while (unsortedLibFiles.length) {
-  const lengthBefore = unsortedLibFiles.length;
-  unsortedLibFiles = unsortedLibFiles.filter( ([name,content]) => {
-    if ([...content.matchAll(importRegexp)].some( importMatch =>
-      orderedLibFiles.findIndex( ([n]) => n === importMatch[2] ) === -1
-    )) return true;
-    orderedLibFiles.push([name,content]);
-  });
-  if (lengthBefore === unsortedLibFiles.length) throw new Error(`circular reference ${unsortedLibFiles.map( ([n]) => n )}`);
+function readFilesInDir(dir) {
+  const files = {};
+  for (const fileName of fs.readdirSync(dir)) {
+    files[fileName] = fs.readFileSync(path.join(dir,fileName),{encoding:"utf8"});
+  }
+  return files;
 }
 
-function genLibFileAdder([name,content]) {
+function orderJsFiles(files) {
+  const sortedFiles = [];
+  let unsortedFiles = Object.entries(files);
+  
+  while (unsortedFiles.length) {
+    const lengthBefore = unsortedFiles.length;
+    unsortedFiles = unsortedFiles.filter( ([name,content]) => {
+      if ([...content.matchAll(importRegexp)].some( importMatch =>
+        sortedFiles.findIndex( ([n]) => n === importMatch[2] ) === -1
+      )) return true;
+      sortedFiles.push([name,content]);
+    });
+    if (lengthBefore === unsortedFiles.length) throw new Error(`circular reference ${unsortedLibFiles.map( ([n]) => n )}`);
+  }
+  
+  return sortedFiles;
+}
+
+function genFileAdder([name,content]) {
   return `addFile("${name}", genImports\`${
     content
       .replace( /[\\`]|\$(?={)/g, "\\$&" )
@@ -39,33 +36,50 @@ function genLibFileAdder([name,content]) {
   }\`);`
 }
 
-// bundle javascript
-const bundledContent = `
-const objectURLs = Object.create(null);
-
-function addFile(name, src) {
-  const blob = new Blob(src, {type:"text/javascript"});
-  objectURLs[name] = URL.createObjectURL(blob) + "#./" + name;
+function bundleJs(dir) {
+  const files = orderJsFiles(readFilesInDir(dir));
+  const bundledJs = `
+  const objectURLs = Object.create(null);
+  
+  function addFile(name, src) {
+    const blob = new Blob(src, {type:"text/javascript"});
+    objectURLs[name] = URL.createObjectURL(blob) + "#./" + name;
+  }
+  
+  function genImports(strings,...imports) {
+    return strings.map( (str,i) =>
+      imports.length === i ? str : str + objectURLs[imports[i]]
+    );
+  }
+  
+  ${files.map(genFileAdder).join("\n")}
+  
+  import(objectURLs["index.js"]);
+  `;
+  
+  if (["<!--","<script","</script"].some( s => bundledJs.includes(s) )) {
+    throw new Error("can't bundle. please remove any occurences of \"<!--\", \"<script\" or \"</script\"");
+  }
+  
+  return bundledJs;
 }
 
-function genImports(strings,...imports) {
-  return strings.map( (str,i) =>
-    imports.length === i ? str : str + objectURLs[imports[i]]
+function bundle(jsDir,htmlPath,outPath) {
+  let htmlContent = fs.readFileSync(htmlPath,{encoding:"utf8"});
+  
+  const favicon = fs.readFileSync(path.join(__dirname,"page","favicon.svg"),{encoding:"utf8"});
+  htmlContent = htmlContent.replace("./favicon.svg", "data:image/svg+xml," + encodeURIComponent(favicon) );
+  
+  htmlContent = htmlContent.replace(
+    "<script src=\"./lib/index.js\" type=\"module\"></script>",
+    () => `<script>${bundleJs(jsDir)}</script>`,
   );
+  
+  fs.writeFileSync(outPath,htmlContent);
 }
 
-${orderedLibFiles.map(genLibFileAdder).join("\n")}
-
-import(objectURLs["index.js"]);
-`;
-
-if (["<!--","<script","</script"].some( s => bundledContent.includes(s) )) {
-  throw new Error("can't bundle. please remove any occurences of \"<!--\", \"<script\" or \"</script\"");
-}
-
-htmlContent = htmlContent.replace(
-  "<script src=\"./lib/index.js\" type=\"module\"></script>",
-  () => `<script>${bundledContent}</script>`,
+bundle(
+  path.join(__dirname,"page","lib"),
+  path.join(__dirname,"page","sheet.html"),
+  path.join(__dirname,"sheet-bundled.html"),
 );
-
-fs.writeFileSync(path.join(__dirname,"sheet-bundled.html"),htmlContent);
