cfeb0f9e80a175468d379cd854935cb45fec0505
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-09 14:25:37 +0100

    remove need of prefix

--- bundle.js
+++ bundle.js
@@ -1,26 +1,12 @@
 const fs = require("node:fs");
 const path = require("node:path");
 
-const PREFIX = "__DSA_BLOB:";
-
 const libFiles = Object.create(null);
 const libPath = path.join(__dirname,"page","lib");
 
 // read file content
 for (const fileName of fs.readdirSync(libPath)) {
   libFiles[fileName] = fs.readFileSync(path.join(libPath,fileName),{encoding:"utf8"});
-}
-
-// replace import/export expressions
-for (const [name,content] of Object.entries(libFiles)) {
-  if (content.includes(PREFIX)) throw new Error(`used reserved keyword ${JSON.stringify(PREFIX)}`);
-  const lines = content.split(/\r?\n/g);
-  for (var i = 0; i < lines.length; i++) {
-    const match = lines[i].match(/^import (.+?) from ".\/([a-zA-Z\-_]*?.js)";$/);
-    if (!match) break;
-    lines[i] = `import ${match[1]} from "${PREFIX}${match[2]}";`;
-  }
-  libFiles[name] = lines.join("\n");
 }
 
 let htmlContent = fs.readFileSync(path.join(__dirname,"page","sheet.html"),{encoding:"utf8"});
@@ -35,14 +21,13 @@ htmlContent = htmlContent.replace(
 );
 
 // order files so that imports always resolve
-const importRegexp = new RegExp(`${PREFIX}([a-zA-Z\\-_]*?.js)`,"g");
 const orderedLibFiles = [];
 let unsortedLibFiles = Object.entries(libFiles);
 
 while (unsortedLibFiles.length) {
   const lengthBefore = unsortedLibFiles.length;
   unsortedLibFiles = unsortedLibFiles.filter( ([name,content]) => {
-    if ([...content.matchAll(importRegexp)].some( importMatch =>
+    if ([...content.matchAll(/^import .+? from ".\/([a-zA-Z\-_]*?.js)";$/gm)].some( importMatch =>
       orderedLibFiles.findIndex( ([n]) => n === importMatch[1] ) === -1
     )) return true;
     orderedLibFiles.push([name,content]);
@@ -55,7 +40,7 @@ const bundledContent = `\
 let objectURLs = Object.create(null);${
   orderedLibFiles.map( ([name,content]) =>
     `objectURLs[${JSON.stringify(name)}] = URL.createObjectURL(new Blob([\`${
-      content.replace( /[\\`]|\$(?={)/g, m => `\\${m}` ).replace( importRegexp, (m,n) => `\${objectURLs[${JSON.stringify(n)}]}` )
+      content.replace( /[\\`]|\$(?={)/g, m => `\\${m}` ).replace( /^import (.+?) from ".\/([a-zA-Z\-_]*?.js)";$/gm, (m,_1,n) => `import ${_1} from "\${objectURLs[${JSON.stringify(n)}]}"` )
     }\`],{type:"text/javascript"})) + "#./" + ${JSON.stringify(name)};`
   ).join("\n")
 }import(objectURLs["index.js"]);`;
