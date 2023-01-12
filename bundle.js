28ae9658c13b11739c8a40a618f5d8f1f2ce0209
Sertonix <83883937+Sertonix@users.noreply.github.com>
2023-01-12 08:54:22 +0100

    update bundle script

--- bundle.js
+++ bundle.js
@@ -1,87 +1,29 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const importRegexp = /^import (.+?) from ".\/([a-zA-Z\-_]*?.js)";$/gm;
 
-function readFilesInDir(dir) {
-  const files = {};
-  for (const fileName of fs.readdirSync(dir)) {
-    files[fileName] = fs.readFileSync(path.join(dir,fileName),{encoding:"utf8"});
-  }
-  return files;
+const htmlContent = fs.readFileSync(path.join(__dirname,"page","sheet.html"),{encoding:"utf8"});
+const indexJsContent = fs.readFileSync(path.join(__dirname,"page","lib","index.js"),{encoding:"utf8"});
+const libDirPath = path.join(__dirname,"page","lib");
+const libFiles = [];
+for (const name of fs.readdirSync(libDirPath)) {
+  if (name === "index.js") continue;
+  libFiles.push([
+    name,
+    fs.readFileSync(path.join(libDirPath,name),{encoding:"utf8"}),
+  ]);
 }
 
-function orderJsFiles(files) {
-  const sortedFiles = [];
-  let unsortedFiles = Object.entries(files);
-  
-  while (unsortedFiles.length) {
-    const lengthBefore = unsortedFiles.length;
-    unsortedFiles = unsortedFiles.filter( ([name,content]) => {
-      if ([...content.matchAll(importRegexp)].some( importMatch =>
-        sortedFiles.findIndex( ([n]) => n === importMatch[2] ) === -1
-      )) return true;
-      sortedFiles.push([name,content]);
-    });
-    if (lengthBefore === unsortedFiles.length) throw new Error(`circular reference ${unsortedLibFiles.map( ([n]) => n )}`);
-  }
-  
-  return sortedFiles;
+function templateStringify(str) {
+  return "`" + str.replace( /[\\`]|\$(?={)/g, "\\$&" ) + "`";
 }
 
-function genFileAdder([name,content]) {
-  return `addFile("${name}", genImports\`${
-    content
-      .replace( /[\\`]|\$(?={)/g, "\\$&" )
-      .replace( importRegexp, "import $1 from \"${\"$2\"}\";" ) // TODO restrict matching to prevent false positives.
-  }\`);`
-}
-
-function bundleJs(dir) {
-  const files = orderJsFiles(readFilesInDir(dir));
-  const bundledJs = `\
-  (()=>{
-  const objectURLs = Object.create(null);
-  
-  function addFile(name, src) {
-    const blob = new Blob(src, {type:"text/javascript"});
-    objectURLs[name] = URL.createObjectURL(blob) + "#./" + name;
-  }
-  
-  function genImports(strings,...imports) {
-    return strings.map( (str,i) =>
-      imports.length === i ? str : str + objectURLs[imports[i]]
-    );
-  }
-  
-  ${files.map(genFileAdder).join("\n")}
-  
-  import(objectURLs["index.js"]);
-  }();`;
-  
-  if (["<!--","<script","</script"].some( s => bundledJs.includes(s) )) {
-    throw new Error("can't bundle. please remove any occurences of \"<!--\", \"<script\" or \"</script\"");
-  }
-  
-  return bundledJs;
-}
-
-function bundle(jsDir,htmlPath,faviconPath,outPath) {
-  let htmlContent = fs.readFileSync(htmlPath,{encoding:"utf8"});
-  
-  const favicon = fs.readFileSync(faviconPath,{encoding:"utf8"});
-  htmlContent = htmlContent.replace("./favicon.svg", "data:image/svg+xml," + encodeURIComponent(favicon) );
-  
-  htmlContent = htmlContent.replace(
-    "<script src=\"./lib/index.js\" type=\"module\"></script>",
-    () => `<script>${bundleJs(jsDir)}</script>`,
-  );
-  
-  fs.writeFileSync(outPath,htmlContent);
-}
-
-bundle(
-  path.join(__dirname,"page","lib"),
-  path.join(__dirname,"page","sheet.html"),
-  path.join(__dirname,"page","favicon.svg"),
-  path.join(__dirname,"sheet-bundled.html"),
+const bundledIndexJs = indexJsContent.replace(/(?<=\/\*! BUNDLER REPLACE \*\/)[\s\S]*?(?=\/\*! END BUNDLER REPLACE \*\/)/, () =>
+  libFiles.map( ([name,content]) => `fileSystem.addFileFromContent("/dsa/lib/${name}",${templateStringify(content)});` ).join("\n")
 );
+
+const bundledHtmlContent = htmlContent.replace(
+  "<script src=\"./lib/index.js\" type=\"module\"></script>",
+  () => `<script>import(URL.createObjectURL(new Blob([${templateStringify(bundledIndexJs)}],{type:"text/javascript"})));</script>`,
+);
+
+fs.writeFileSync(path.join(__dirname,"sheet-bundled.html"),bundledHtmlContent);
